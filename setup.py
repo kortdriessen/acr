6b518895fd07e6b82d51692ff0c3d940f62ae39b
Kort Driessene <driessenkort@gmail.com>
2022-10-20 19:31:06 -0500

    new setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='acr',
+      version='0.1',
+      description='Analysis tools for ACR optogenetic inhibition',
+      url='http://github.com/kortdriessen/acr',
+      author='Kort Driessen',
+      author_email='driessen2@wisc.edu',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
