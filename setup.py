ce9a11681e36596a6aaa07d8111d3bf5946f1892
Kort Driessene <driessenkort@gmail.com>
2022-10-20 20:27:27 -0500

    quick setup.py fix

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
-setup(name='acr',
-      version='0.1',
-      description='Analysis tools for ACR optogenetic inhibition',
-      url='http://github.com/kortdriessen/acr',
-      author='Kort Driessen',
-      author_email='driessen2@wisc.edu',
-      license='MIT',
-      packages=['acr'],
-      install_requires=['streamlit', 'plotly'] #needs kdephys + ecephys
-      zip_safe=False)
+setup(
+    name="acr",
+    version="0.1",
+    description="Analysis tools for ACR optogenetic inhibition",
+    url="http://github.com/kortdriessen/acr",
+    author="Kort Driessen",
+    author_email="driessen2@wisc.edu",
+    license="MIT",
+    packages=["acr"],
+    install_requires=["streamlit", "plotly"],  # needs kdephys + ecephys
+    zip_safe=False,
+)
