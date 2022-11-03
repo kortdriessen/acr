9109e7c04594efe20167a32acef653863ec18e9e
Kort Driessene <driessenkort@gmail.com>
2022-11-03 18:43:30 -0500

    moved notebooks to lab_analysis folder

--- notebooks/archive/strmlt_scratch.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import tdt
-import kd_analysis.main.utils_pandas as kup
-
-path = '/Volumes/opto_loc/Data/ACR_9/ACR_9-control1'
-data = kup.tdt_to_pandas(path, t1=0, t2=1200, channel=[1,2], store='EEGr')
-
-avg = data['1'].mean(axis=0)
-st.write(avg)
