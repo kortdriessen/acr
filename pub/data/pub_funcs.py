8723e82c416317d898c8ccb58beed78fa93639a3
Kort Driessene <driessenkort@gmail.com>
2024-08-01 17:22:57 -0500

    pub_func updates

--- pub/data/pub_funcs.py
+++ pub/data/pub_funcs.py
@@ -45,39 +45,10 @@ def get_sd_df(subject, exp, reprocess_existing=False, save=True):
         else:
             os.remove(f'./sd_data/{subject}--{exp}--sd.parquet')
     
-    # load some basic information, and the hypnogram
-    h = acr.io.load_hypno_full_exp(subject, exp, update=False)
-    rec_times = acr.info_pipeline.subject_info_section(subject, 'rec_times')
     params = acr.info_pipeline.subject_params(subject)
     stores = params['time_stores']
-    sort_ids = [f'{exp}-{store}' for store in stores]
     recordings = acr.info_pipeline.get_exp_recs(subject, exp)
-    
-    # load some temporal information about the rebound, baseline, sd, etc. 
-    stim_start, stim_end = acr.stim.stim_bookends(subject, exp)
-    reb_start = h.hts(stim_end-pd.Timedelta('15min'), stim_end+pd.Timedelta('1h')).st('NREM').iloc[0].start_time
-    if reb_start < stim_end:
-        stim_end_hypno = h.loc[(h.start_time<stim_end)&(h.end_time>stim_end)] # if stim time is in the middle of a nrem bout, then it can be the start of the rebound
-        if stim_end_hypno.state.values[0] == 'NREM':
-            reb_start = stim_end
-        else:
-            raise ValueError('Rebound start time is before stim end time, need to inspect')
-
-    assert reb_start >= stim_end, 'Rebound start time is before stim end time'
-
-    bl_start_actual = rec_times[f'{exp}-bl']["start"]
-    bl_day = bl_start_actual.split("T")[0]
-    bl_start = pd.Timestamp(bl_day + "T09:00:00")
-
-    if f'{exp}-sd' in rec_times.keys():
-        sd_rec = f'{exp}-sd'
-        sd_end = pd.Timestamp(rec_times[sd_rec]['end'])
-    else:
-        sd_rec = exp
-        sd_end = stim_start
-    sd_start_actual = pd.Timestamp(rec_times[sd_rec]['start'])
-    sd_day = rec_times[sd_rec]['start'].split("T")[0]
-    sd_start = pd.Timestamp(sd_day + "T09:00:00")
+    sd_start, stim_start, stim_end, reb_start, exp_start = acr.info_pipeline.get_sd_exp_landmarks(subject, exp)
     
     # Load the BANDPOWER DATA
     #-------------------------------
@@ -149,39 +120,10 @@ def get_rebound_df(subject, exp, reprocess_existing=False, save=True):
         else:
             os.remove(f'./rebound_data_1h/{subject}--{exp}--reb.parquet')
 
-    # load some basic information, and the hypnogram
-    h = acr.io.load_hypno_full_exp(subject, exp, update=False)
-    rec_times = acr.info_pipeline.subject_info_section(subject, 'rec_times')
     params = acr.info_pipeline.subject_params(subject)
     stores = params['time_stores']
-    sort_ids = [f'{exp}-{store}' for store in stores]
     recordings = acr.info_pipeline.get_exp_recs(subject, exp)
-    
-    # load some temporal information about the rebound, baseline, sd, etc. 
-    stim_start, stim_end = acr.stim.stim_bookends(subject, exp)
-    reb_start = h.hts(stim_end-pd.Timedelta('15min'), stim_end+pd.Timedelta('1h')).st('NREM').iloc[0].start_time
-    if reb_start < stim_end:
-        stim_end_hypno = h.loc[(h.start_time<stim_end)&(h.end_time>stim_end)] # if stim time is in the middle of a nrem bout, then it can be the start of the rebound
-        if stim_end_hypno.state.values[0] == 'NREM':
-            reb_start = stim_end
-        else:
-            raise ValueError('Rebound start time is before stim end time, need to inspect')
-
-    assert reb_start >= stim_end, 'Rebound start time is before stim end time'
-
-    bl_start_actual = rec_times[f'{exp}-bl']["start"]
-    bl_day = bl_start_actual.split("T")[0]
-    bl_start = pd.Timestamp(bl_day + "T09:00:00")
-
-    if f'{exp}-sd' in rec_times.keys():
-        sd_rec = f'{exp}-sd'
-        sd_end = pd.Timestamp(rec_times[sd_rec]['end'])
-    else:
-        sd_rec = exp
-        sd_end = stim_start
-    sd_start_actual = pd.Timestamp(rec_times[sd_rec]['start'])
-    sd_day = rec_times[sd_rec]['start'].split("T")[0]
-    sd_start = pd.Timestamp(sd_day + "T09:00:00")
+    sd_start, stim_start, stim_end, reb_start, exp_start = acr.info_pipeline.get_sd_exp_landmarks(subject, exp)
     
     # Load the BANDPOWER DATA
     #-------------------------------
